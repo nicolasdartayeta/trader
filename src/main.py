f36438683c84a00c9471f6f9df6ad468a1630bb4
Nicolas Dartayeta <63559546+nicolasdartayeta@users.noreply.github.com>
2024-11-19 09:02:23 -0300

    estrategia rustica y grafico

--- src/main.py
+++ src/main.py
@@ -7,31 +7,14 @@ import sys  # To find out the script name (in argv[0])
 
 # Import the backtrader platform
 import backtrader as bt
-
-
-# Create a Stratey
-class TestStrategy(bt.Strategy):
-
-    def log(self, txt, dt=None):
-        ''' Logging function for this strategy'''
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
-
-    def __init__(self):
-        # Keep a reference to the "close" line in the data[0] dataseries
-        self.dataclose = self.datas[0].close
-
-    def next(self):
-        # Simply log the closing price of the series from the reference
-        self.log('Close, %.2f' % self.dataclose[0])
-
+from estrategia import (TestStrategy)
 
 if __name__ == '__main__':
     # Create a cerebro entity
     cerebro = bt.Cerebro()
 
     # Add a strategy
-    cerebro.addstrategy(TestStrategy)
+    cerebro.addstrategy(TestStrategy, exitbars=20)
 
     # Datas are in a subfolder of the samples. Need to find where the script is
     # because it could have been called from anywhere
@@ -51,14 +34,22 @@ if __name__ == '__main__':
     # Add the Data Feed to Cerebro
     cerebro.adddata(data)
 
+    # 0.1% ... divide by 100 to remove the %
+    cerebro.broker.setcommission(commission=0.001)
+
     # Set our desired cash start
     cerebro.broker.setcash(100000.0)
 
     # Print out the starting conditions
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-
+    initValue = cerebro.broker.getvalue()
     # Run over everything
     cerebro.run()
 
+
     # Print out the final result
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    finalValue = cerebro.broker.getvalue()
+
+    print('Profit: %.2f' % (finalValue - initValue))
+    cerebro.plot()
